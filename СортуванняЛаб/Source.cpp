3422ebe20e0af8393cc8ec207260d6e365416206
OlhaPianova <piolmaolga@gmail.com>
2020-03-20 21:05:35 +0200

    commit3

--- СортуванняЛаб/Source.cpp
+++ СортуванняЛаб/Source.cpp
@@ -1,7 +1,11 @@
 #include<iostream>
 #include<ctime>
+#include <Windows.h>
 using namespace std;
-int index = 0;
+#define CLEAR system("cls");
+#define SLEEP Sleep(50);
+int kroc = 0;
+
 
 void RandArr(int array[],int size) {
 	srand(time(0));
@@ -12,7 +16,6 @@ void RandArr(int array[],int size) {
 void ShowArray(int array[], int size) {
 	for (int i = 0; i < size; i++) {
 		cout << array[i] << " ";
-		index++;
 	}
 	cout << endl;
 }
@@ -24,23 +27,28 @@ void ShowElement(int array[], int size) {
 		cout << "Error: invalid number!" << endl;
 	}
 	else {
-			cout << element-1 <<" "<< array[element - 1] << endl;
+			cout<<"Index: " << element-1 <<","<<" "<<"number: "<< array[element - 1] << endl;
 	}
 }
-
-void SortZrost_bubble(int array[], int size,int spadZrost,int sposib) {
-
+void SortZrost_bubble(int array[], int size) {
 		for (int i = 0; i < size - 1; i++) {
 			for (int j = 0; j < size - 1; j++) {
 				if (array[j]>array[j + 1]) {
 					int temp = array[j];
 					array[j] = array[j + 1];
 					array[j + 1] = temp;
+					kroc++;
+					for (int i = 0; i < size; i++) {
+						cout << array[i] << " ";
+					}
+					SLEEP;
+					CLEAR;
 				}
 			}
 		}
+		cout << "number of steps: " << kroc << endl;
 }
-void SortZrost_select(int array[], int size, int spadZrost, int sposib) {
+void SortZrost_select(int array[], int size) {
 	for (int i = 0; i < size; i++) {
 		int temp = array[0];
 		for (int j = i + 1; j < size; j++) {
@@ -48,11 +56,18 @@ void SortZrost_select(int array[], int size, int spadZrost, int sposib) {
 				temp = array[i];
 				array[i] = array[j];
 				array[j] = temp;
+				kroc++;
 			}
+			for (int i = 0; i < size; i++) {
+				cout << array[i] << " ";
+			}
+			SLEEP;
+			CLEAR;
 		}
 	}
+	cout << "number of steps: " << kroc << endl;
 }
-void SortZrost_insert(int array[], int size, int spadZrost, int sposib) {
+void SortZrost_insert(int array[], int size) {
 	int temp = 0;
 	int indexPr = 0;
 	for (int i = 1; i < size; i++) {
@@ -62,21 +77,107 @@ void SortZrost_insert(int array[], int size, int spadZrost, int sposib) {
 			array[indexPr + 1] = array[indexPr];
 			array[indexPr] = temp;
 			indexPr--;
+			kroc++;
+			for (int i = 0; i < size;i++) {
+				cout << array[i] << " ";
+			}
+			SLEEP;
+			CLEAR;
 		}
 	}
+	cout << "number of steps: " << kroc << endl;
+}
+void SortSpad_bubble(int array[],int size) {
+	for (int i = size; i >0; --i) {
+		for (int j = size; j >0; --j) {
+			if (array[j]>array[j - 1]) {
+				int temp = array[j];
+				array[j] = array[j - 1];
+				array[j - 1] = temp;
+				kroc++;
+			}
+			for (int i = 0; i < size; i++) {
+				cout << array[i] << " ";
+			}
+			SLEEP;
+			CLEAR;
+		}
+	}
+	cout << "number of steps: " << kroc << endl;
+}
+void SortSpad_select(int array[], int size) {
+	for (int i = size; i >=0; --i) {
+		int temp = array[0];
+		for (int j = i - 1; j >=0; --j) {
+			if (array[i]>array[j]) {
+				temp = array[i];
+				array[i] = array[j];
+				array[j] = temp;
+				kroc++;
+			}
+			for (int i = 0; i < size; i++) {
+				cout << array[i] << " ";
+			}
+			SLEEP;
+			CLEAR;
+		}
+	}
+	cout << "number of steps: " << kroc << endl;
+}
+void SortSpad_insert(int array[], int size) {
+	int temp = 0;
+	int indexPr = 0;
+
+	for (int i = 1; i < size; i++) {
+		temp = array[i];
+		indexPr = i - 1;
+		while (indexPr >= 0 && array[indexPr]<temp) {
+			array[indexPr + 1] = array[indexPr];
+			array[indexPr] = temp;
+			indexPr--;
+			kroc++;
+			for (int i = 0; i < size; i++) {
+				cout << array[i] << " ";
+			}
+			SLEEP;
+			CLEAR;
+		}
+	}
+	cout << "number of steps: " << kroc << endl;
+}
+int Seredne(int array[],int size,int sum,int seredne) {
+	int index = 0;
+	for (int i = 0; i < size; i++) {
+		 index ++;
+		sum = sum + array[i];
+		seredne = sum / index;
+	}
+	cout << "average arithmetic: " << seredne << endl;
+	return seredne;
+}
+void kratne(int array[], int size) {
+	cout << "\tKratni 5: " << endl;
+	for (int i = 0; i < size; i++) {
+		if (array[i]%5==0) {
+			cout << array[i] << " ";
+		}
+	}
+	cout << endl;
 }
 int main() {
-	const int size = 100;
+	const int size = 15;
 	int Masyv[size];
+	int sum = 0;
+	int seredne = 0;
 	int action = 0;
 	do {
-		cout << "\t\t\tSelect array manipulation: \n\n";
+		cout << "\n\t\t\tSelect array manipulation: \n\n";
 		cout << "1. Fill in an array with random numbers" << endl;
 		cout << "2. Show array" << endl;
 		cout << "3. Show the index of the array element you want " << endl;
 		cout << "4. Sort array" << endl;
 		cout << "5. Show average arithmetic" << endl;
-		cout << "6. Sort elements rkatni 5" << endl;
+		cout << "6. Sort elements kratni 5" << endl;
 		cout << "7. Exit" << endl;
 		cin >> action;
 
@@ -85,38 +186,77 @@ int main() {
 			RandArr(Masyv, size);
 		}break;
 		case 2: {
+			CLEAR;
 			ShowArray(Masyv, size);
 		}break;
 		case 3: {
+			CLEAR;
+			ShowArray(Masyv, size);
+			cout << endl;
 			ShowElement(Masyv, size);
 		}break;
 		case 4: {
-			cout << "Select  sort: " << endl;
-			cout << "1- Po zrostannu " << endl;
-			cout << "2- Po spadannu " << endl;
-			int spadZrost = 0;
-			cin >> spadZrost;
+			CLEAR;
 			cout << "Choose one of the sorting methods: " << endl;
-			cout << "1. bubble" << endl;
-			cout << "2. select" << endl;
-			cout << "3. insert" << endl;
-			int sposib = 0;
-			cin >> sposib;
-			if (spadZrost==1&& sposib==1) {
-				SortZrost_bubble(Masyv, size, spadZrost, sposib);
+			cout << endl;
+			cout << "1- Po zrostannu, bubble method " << endl;
+			cout << "2- Po zrostannu, select method " << endl;
+			cout << "3- Po zrostannu, insert method " << endl;
+			cout << "4- Po spadannu, bubble method " << endl;
+			cout << "5- Po spadannu, select method " << endl;
+			cout << "6- Po spadannu, insert method " << endl;
+			int method = 0;
+			cin >> method;
+			switch (method) {
+			case 1: {
+				CLEAR;
+				SortZrost_bubble(Masyv, size);
+				ShowArray(Masyv, size);
+				cout << endl;
+			}break;
+			case 2: {
+				CLEAR;
+				SortZrost_select(Masyv, size);
+				ShowArray(Masyv, size);
+				cout << endl;
+			}break;
+			case 3: {
+				CLEAR;
+				SortZrost_insert(Masyv, size);
+				ShowArray(Masyv, size);
+				cout << endl;
+			}break;
+			case 4: {
+				CLEAR;
+				SortSpad_bubble(Masyv, size);
+				ShowArray(Masyv, size);
+				cout << endl;
+			}break;
+			case 5: {
+				CLEAR;
+				SortSpad_select(Masyv, size);
+				ShowArray(Masyv, size);
+				cout << endl;
+			}break;
+			case 6: {
+				CLEAR;
+				SortSpad_insert(Masyv, size);
+				ShowArray(Masyv, size);
+				cout << endl;
+			}break;
 			}
-			else if (spadZrost == 1 && sposib == 2) {
-				SortZrost_select(Masyv, size, spadZrost,sposib);
-			}
-			else if (spadZrost == 1 && sposib == 3) {
-				SortZrost_insert(Masyv, size, spadZrost, sposib);
-			}
+			
 		}break;
 		case 5: {
-
+			CLEAR;
+			ShowArray(Masyv, size);
+			cout << endl;
+			Seredne(Masyv, size, sum, seredne);
+			cout << endl;
 		}break;
 		case 6: {
-
+			CLEAR;
+			kratne(Masyv, size);
 		}break;
 		case 7: {
 			cout << "BYE!" << endl;
